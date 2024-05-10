6eb5d7710f0db5ca20500c9ea8d9389401609c61
Binnur KURT <deepcloudlabs@gmail.com>
2024-05-10 16:09:39 +0300

    initial commit

---
+++ lottery-microservice-with-jwt/src/main/java/com/example/lottery/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.lottery.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.lottery.filter.JwtTokenFilter;
+
+@Configuration
+public class WebSecurityConfig {
+	@Value("${secretKey}")
+	private String secret;
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http, UserDetailsService userDetailsService,AuthenticationManager authenticationManager) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeRequests(requests -> requests.antMatchers("/api/v1/login").permitAll().anyRequest().authenticated()).sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.authenticationManager(authenticationManager);
+		http.userDetailsService(userDetailsService);
+		http.addFilterBefore(new JwtTokenFilter(userDetailsService, secret),UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+
+	@Bean
+	UserDetailsManager userDetailsManager() {
+		UserDetails user = User.withUsername("jack").password("secret").roles("USER","ADMIN").build();
+		return new InMemoryUserDetailsManager(user);
+	}
+}
